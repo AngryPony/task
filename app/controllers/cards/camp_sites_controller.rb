9f96825d9499b0fa57e9bf54e2ebac5d18a2f964
AngryPony <51092422+AngryPony@users.noreply.github.com>
2021-06-09 19:53:51 +0300

    добавлен контроллер для главной страницы

---
+++ app/controllers/cards/camp_sites_controller.rb
@@ -0,0 +1,28 @@
+class Cards::CampSitesController < CampSitesController
+
+  def index
+    @campsites = Campsite.all
+
+    unless params[:country_id].nil?
+      @country = Country.find(params[:country_id])
+      @campsites = @country.campsites
+    end
+
+    unless params[:region_id].nil?
+      @region = Region.find(params[:region_id])
+      @campsites = @region.campsites
+    end
+
+    unless params[:city_id].nil?
+      @city = City.find(params[:city_id])
+      @campsites = @city.campsites
+    end
+
+    @countries = Country.all
+    @regions = @country.nil? ? Region.all : @country.regions
+    @cities = @region.nil? ? City.all : @region.cities
+  end
+
+
+
+end
